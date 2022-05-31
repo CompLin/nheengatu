fbe884ea05091c6f5993254e83c8ca91d79d05be
leoalenc <leonel.de.alencar@ufc.br>
2022-05-31 12:21:38 -0300

    resolve #54

---
+++ src/Nheengatagger.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# This script applies the Nheengatagger.py Python module to a list
+# of files in the data/corpus/navarro-2016 folder.
+
+# Usage example:
+# Nheengatagger.sh *[0-9][0-9].txt
+
+# Author: Leonel Figueiredo de Alencar
+# Date May 31, 2022
+
+for i in "$@"
+do
+
+set $(echo "$i" | awk 'BEGIN {FS= "."} { print $1}')
+
+Nheengatagger.py "$i" > "$1"-pos.txt
+done
