ec0bfe1625d794308f835db3f88dc3089037e8bb
leoalenc <leonel.de.alencar@ufc.br>
2024-06-18 18:05:34 -0300

    tarefa 1 #434

---
+++ src/Metadata.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Leonel Figueiredo de Alencar
+# Last update: June 16, 2024
+
+def Mindlin():
+	person="Gabriela Lourenço Fernandes, Biblioteca Brasiliana Guita e José Mindlin"
+	transcriber={'text_orig_transcriber': person,
+	'text_por_modernizer': person}
+	return transcriber
